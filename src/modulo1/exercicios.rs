d09195c31e8d2ea4c348110e2cfe82714c33e4d7
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2025-01-06 15:59:21 -0300

    chome: detalhes do fluxo

--- src/modulo1/exercicios.rs
+++ src/modulo1/exercicios.rs
@@ -8,9 +8,21 @@ pub fn exercicio_1() {
 
     let ano_nascimento: u16 = 1978;
     let mes_nascimento: u16 = 08;
-    let ano_atual: u16 = 2025;
+    let dia_nascimento: u16 = 18;
 
-    let idade = ano_atual - ano_nascimento;
+    let ano_atual: u16 = 2025;
+    let mes_autal: u16 = 1;
+    let dia_atual: u16 = 6;
+
+    let mut idade = ano_atual - ano_nascimento;
+
+    if mes_nascimento > mes_autal {
+        idade -= 1;
+    } else if mes_nascimento == mes_autal && dia_nascimento > dia_atual {
+        idade -= 1;
+    } else {
+        idade = idade;
+    }
 
     println!("A idade do ({}) calculado para o ano de {} é de {}!", nome, ano_nascimento, idade);
 }
